2e70ec0bb4444648207e279dba3f126b4b78cb98
ackermanjayjay <reza23198@gmail.com>
2023-12-22 17:05:13 +0700

    fix(change layouts)

--- resources/views/Pages/homepages.blade.php
+++ resources/views/Pages/homepages.blade.php
@@ -1,42 +1,49 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{$judul}}</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  </head>
-  <body>
+    <title>{{ $judul }}</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body>
     <div class="d-flex flex justify-content-center mt-5">
-        <h1>{{$kontent}}</h1>
-      </div>
-      
+        <h1>{{ $kontent }}</h1>
+    </div>
+
     <form action="/cari" method="GET">
-      @csrf
-      <div class="input-group input-group-lg container d-flex flex justify-content-center mt-5">
-        <input type="text" class="form-control" aria-label="Sizing example input"  placeholder="Cari barang" aria-describedby="inputGroup-sizing-lg" name="search">
-      </div>
-        </form>
+        @csrf
+        <div class="input-group input-group-lg container d-flex flex justify-content-center mt-5">
+            <input type="text" class="form-control" aria-label="Sizing example input" placeholder="Cari barang"
+                aria-describedby="inputGroup-sizing-lg" name="search">
+        </div>
+    </form>
 
     <div class="container d-flex flex justify-content-center mt-5">
         <div class="row">
             @foreach ($result as $item)
-            <div class="col">
-                <div class="card" style="width: 18rem;">
-                    <img src={{$item['images'][0]}} class="card-img-top" alt="...">
-                    <div class="card-body">
-                      <h5 class="card-title">{{$item['title']}}</h5>
-                      <p class="card-text">{{$item['description']}}</p>
-                      <p class="text-primary">${{$item['price']}}</p>
-                      <p class="text-black">Category: {{$item['category']}}</p>
-                      <a href="/show/{{$item['id']}}" class="btn btn-primary">Details</a>
+                <div class="col">
+                    <div class="card" style="width: 15rem;">
+                        <img src={{ $item['images'][0] }} class="card-img-top object-fit-cover w-100"
+                            style=" height: 200px;    
+                    " alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $item['title'] }}</h5>
+                            <p class="text-primary">${{ $item['price'] }}</p>
+                            <a href="/show/{{ $item['id'] }}" class="btn btn-primary">Details</a>
+                        </div>
                     </div>
-                  </div>
-            </div>
+                </div>
             @endforeach
         </div>
-      </div>
+    </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-  </body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+</body>
+
 </html>
--- resources/views/Pages/products.blade.php
+++ resources/views/Pages/products.blade.php
@@ -4,29 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Detail Data Post - SantriKoding.com</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-</head>
+    <title>Detail Data</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"></head>
 <body style="background: lightgray">
 
-    <div class="container mt-5 mb-5">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card border-0 shadow-sm rounded">
-                    <div class="card-body">
-                        <img src={{$result['images'][0]}} class="w-100 rounded">
-                        <hr>
-                        <h4>{{ $result['title'] }}</h4>
-                        <p class="tmt-3">
-                            {!! $result['description'] !!}
-                        </p>
-                    </div>
-                </div>
+    <div class="d-flex justify-content-center mt-5 rounded-xl">
+    <div class="card" style="width: 18rem;">
+            <img src={{$result['images'][0]}} class="card-img-top rounded-xl w-100 object-fit-cover" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">{{$result['title']}}</h5>
+                <p class="card-text">{{$result['description']}}</p>
+                <p class="card-text">${{$result['price']}}</p>
+                <a href="#" class="btn btn-success">Buy</a>
             </div>
         </div>
-    </div>
+      </div>
     
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+</script>
 </body>
 </html>
